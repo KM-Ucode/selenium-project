13ab9a67f60f7e10f79b8317ce3a351953255c88
KM-Ucode <hmuminova@gmail.com>
2023-11-05 00:28:53 -0400

    Homework

--- src/test/java/com/ucode_academy/test/day_02_selenium_locators/Homework.java
+++ src/test/java/com/ucode_academy/test/day_02_selenium_locators/Homework.java
@@ -1,6 +1,8 @@
 package com.ucode_academy.test.day_02_selenium_locators;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class Homework {
@@ -8,11 +10,30 @@ public class Homework {
 
         WebDriver driver = new ChromeDriver();
 
-        driver.get("https://www.etsy.com/");
+        driver.get("https://www.google.com/");
 
         driver.manage().window().maximize();
 
         Thread.sleep(3000);
+
+        driver.navigate().to("https://www.etsy.com/");
+
+        Thread.sleep(3000);
+
+        WebElement searchButton = driver.findElement(By.id("global-enhancements-search-query"));
+        searchButton.sendKeys("Java");
+        searchButton.click();
+
+        Thread.sleep(3000);
+
+        String actualTitle = driver.getTitle();
+
+        System.out.println("Title of the page: " + actualTitle);
+
+        driver.quit();
+
+
+
 
     }
 }
