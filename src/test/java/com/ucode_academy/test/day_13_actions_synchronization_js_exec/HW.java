c21c8c569b4cca18ff25fd8c487cbb90230f636f
KM-Ucode <hmuminova@gmail.com>
2023-11-28 14:00:12 -0500

    new package & class  added

---
+++ src/test/java/com/ucode_academy/test/day_13_actions_synchronization_js_exec/HW.java
@@ -0,0 +1,16 @@
+package com.ucode_academy.test.day_13_actions_synchronization_js_exec;
+
+public class HW {
+    //Task 1:
+    //
+    //1: Go to https://www.demoblaze.com/index.html
+    //2: Click on Login button enter user name: test & pwd: test
+    //3: Click on login button
+    //4: Click on a product (any product) & clcik on Add to car button
+    //5: Verify product was added success (JS alert)
+    //6: Click on Car on the top of the page
+    //7: Click on Place Order & fill all the fields (any number for car number)
+    //8: Click on Purchase & verify order was successful
+    // Note: After clicking On Purchase "Thank you for your purchase" will display
+    //       Grab order info and click on Ok
+}
